10e6d8a2084b967c4547c5bbcfccb9e12d0dfff5
Javed Khan <tuxcanfly@gmail.com>
2023-05-31 11:01:46 -0700

    docker: update celestia-app script

--- integration_test/docker/start-bridge.sh
+++ integration_test/docker/start-bridge.sh
@@ -5,7 +5,7 @@ apk add curl jq
 
 celestia bridge init --node.store /bridge
 /wait-for-it.sh 192.167.10.10:26657 -t 90 -- \
-  sleep 10 && curl -s http://192.167.10.10:26657/block?height=1 | jq '.result.block_id.hash' | tr -d '"' > genesis.hash
+  curl -s http://192.167.10.10:26657/block?height=1 | jq '.result.block_id.hash' | tr -d '"' > genesis.hash
 
 curl -s http://192.167.10.10:26657/block_by_hash?hash=0x`cat genesis.hash`
 echo  # newline
